c490947d6c853a8f491856c884a48f05ed210fba
Emil Sayahi <32737166+limeschool@users.noreply.github.com>
2020-11-10 01:00:54 -0500

    Update update-pacman.sh

--- update-pacman.sh
+++ update-pacman.sh
@@ -2,5 +2,6 @@ pip freeze --user | cut -d'=' -f1 | xargs -n1 pip install -U
 rustup update
 flatpak update -y
 sudo snap refresh
+paru -Sua --noconfirm
 sudo pacman -Syu --noconfirm
 pacman -Qtdq | pacman -Rns -
