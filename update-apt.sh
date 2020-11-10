d41be4a6d0c4a4c617ecef8f95bf6a1b4e9f2287
Emil Sayahi <32737166+limeschool@users.noreply.github.com>
2020-11-10 00:48:25 -0500

    Create update-apt.sh

---
+++ update-apt.sh
@@ -0,0 +1,10 @@
+rustup update
+sudo apt update
+flatpak update -y
+sudo snap refresh
+sudo dpkg --configure -a
+sudo apt --fix-broken install -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
